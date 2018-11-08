efed1c15ee8f957f905d82bd57db867554526d98
klupois <klupois@gmail.com>
2018-11-08 16:14:38 +0300

    1st commitprogram to sum divible numbers between 1 and 100

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -10,6 +10,19 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
+            int a, b, i, Sum = 0;
+            for (i = 1; i < 100; i++)
+            {
+                a = i % 3;
+                b = i % 5;
+                if (a == 0 || b == 0)
+                {
+                    Console.Write("{0}\t", i);
+                    Sum = Sum + i;
+                }
+            }
+            Console.WriteLine("\nThe Sum of all the Multiples of 3 or 5 Below 100 : {0}", Sum);
+            Console.Read();
         }
     }
 }
