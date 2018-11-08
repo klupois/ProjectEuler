fc9b76f022c99343d6e5fdf0ae060deddebed9cc
klupois <klupois@gmail.com>
2018-11-08 18:22:23 +0300

    adding fibonacci code

---
+++ Project2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c=0;
+            int i = 0;
+            int sum = 0;
+
+            //Console.Write("{0}\t", a);
+            //Console.Write("{0}\t", b);
+            
+          while(c< 50)
+            {
+                if (c % 2 == 0)
+                {
+                    Console.Write("{0}\t", c);
+                    sum = sum + c;
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+                //Console.Write("{0}\t", c);
+            }
+            Console.Write("{0}\t", sum);
+
+            Console.Read();
+
+        }
+    }
+}
