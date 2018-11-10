12dd4b7929dac6841b8100423fbbe219caa0bdeb
Caleb Kimani (Techno Brain Global FZE) <v-cakima@microsoft.com>
2018-11-10 10:26:11 +0300

    adding program for prime numbers

---
+++ Project_Prime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i, Sum = 0;
+
+            for (i = 1; i < 100; i++)
+            {
+                if (100 % 2 == 0)
+                {
+
+                    a = 100 / i;
+                    Console.Write("{0}\t",a);
+                }
+
+                /*a = i % 3;
+                b = i % 5;
+                if (a == 0 || b == 0)
+                {
+                    Console.Write("{0}\t", i);
+                    Sum = Sum + i;
+                } */
+            }
+            //Console.WriteLine("\nThe Sum of all the Multiples of 3 or 5 Below 100 : {0}", Sum);
+            Console.Read();
+        }
+    }
+}
